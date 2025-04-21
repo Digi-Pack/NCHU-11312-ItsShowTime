677d4dd78c10cef13cae09ab23e9e6284ccb9fe3
Angela <111516733+Angela0405@users.noreply.github.com>
2025-04-21 13:48:53 +0800

    feat: 新增路徑

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,12 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
+    return Inertia::render('frontend/homePage');
+})->name('home.page');
+
+// Route::get('/', function () {
+//     return Inertia::render('Welcome');
+// })->name('home');
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
