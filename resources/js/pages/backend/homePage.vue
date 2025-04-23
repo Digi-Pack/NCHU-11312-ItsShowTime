ba27c8bb5c9dff87bc83a7d957b1b4ea500cdbfa
Serina-Cheng <100753000+Serina-Cheng@users.noreply.github.com>
2025-04-23 19:16:29 +0800

    feat:新增後台

---
+++ resources/js/pages/backend/homePage.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref } from 'vue'
+
+
+
+</script>
+
+<template>
+
+    <section class="py-10 px-4">
+        <div class="flex ">
+            <p class="text-3xl pl-4 mb-10">後台管理</p>
+            <button type="button">新增</button>
+        </div>
+
+        <div class="overflow-x-auto p-4">
+            <table class="min-w-full border border-gray-300">
+                <thead class="bg-gray-200 text-gray-700">
+                    <tr>
+                        <th class="px-4 py-2 text-left text-sm md:text-base xl:text-lg 2xl:text-xl">新增日期</th>
+                        <th class="px-4 py-2 text-left text-sm md:text-base xl:text-lg 2xl:text-xl">發布日期</th>
+                        <th class="px-4 py-2 text-left text-sm md:text-base xl:text-lg 2xl:text-xl">標題</th>
+                        <th class="px-4 py-2 text-left text-sm md:text-base xl:text-lg 2xl:text-xl">操作</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="border-t">
+                        <td class="px-4 py-2 text-sm md:text-base xl:text-lg 2xl:text-xl">資料 1-1</td>
+                        <td class="px-4 py-2 text-sm md:text-base xl:text-lg 2xl:text-xl">資料 1-2</td>
+                        <td class="px-4 py-2 text-sm md:text-base xl:text-lg 2xl:text-xl">資料 1-3</td>
+                        <td class="px-4 py-2 text-sm md:text-base xl:text-lg 2xl:text-xl">資料 1-4</td>
+                    </tr>
+                    <tr class="border-t">
+                        <td class="px-4 py-2 text-sm md:text-base xl:text-lg 2xl:text-xl">資料 2-1</td>
+                        <td class="px-4 py-2 text-sm md:text-base xl:text-lg 2xl:text-xl">資料 2-2</td>
+                        <td class="px-4 py-2 text-sm md:text-base xl:text-lg 2xl:text-xl">資料 2-3</td>
+                        <td class="px-4 py-2 text-sm md:text-base xl:text-lg 2xl:text-xl">資料 2-4</td>
+                    </tr>
+                    <tr class="border-t">
+                        <td class="px-4 py-2 text-sm md:text-base xl:text-lg 2xl:text-xl">資料 3-1</td>
+                        <td class="px-4 py-2 text-sm md:text-base xl:text-lg 2xl:text-xl">資料 3-2</td>
+                        <td class="px-4 py-2 text-sm md:text-base xl:text-lg 2xl:text-xl">資料 3-3</td>
+                        <td class="px-4 py-2 text-sm md:text-base xl:text-lg 2xl:text-xl">資料 3-4</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </section>
+
+
+</template>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return Inertia::render('frontend/homePage');
 })->name('home.page');
 
+Route::get('/backend', function () {
+    return Inertia::render('backend/homePage');
+})->name('backend.home.page');
+
 // Route::get('/', function () {
 //     return Inertia::render('Welcome');
 // })->name('home');
@@ -17,3 +21,5 @@ Route::get('dashboard', function () {
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
+// require __DIR__.'/frontend.php';
+// require __DIR__.'/backend.php';
