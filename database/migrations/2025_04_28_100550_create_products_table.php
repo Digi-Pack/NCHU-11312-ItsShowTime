0144c9b05f98d7017fcbd10cf853c1669fb51766
Angela <111516733+Angela0405@users.noreply.github.com>
2025-04-28 11:38:05 +0800

    feat:加入詢價

---
+++ database/migrations/2025_04_28_100550_create_products_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->comment('商品名稱');
+            $table->text('img_url')->comment('商品圖片URL');
+            $table->unsignedSmallInteger('price')->comment('商品價格');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
