baba8a767976e3d7db8681c7ed5b8cf1b21099f1
Angela <111516733+Angela0405@users.noreply.github.com>
2025-05-01 19:30:32 +0800

    fixed: banner後台

--- resources/js/pages/backend/banner/BannerEdit.vue
+++ resources/js/pages/backend/banner/BannerEdit.vue
@@ -10,8 +10,43 @@ const item = ref({
     'img_path': props.response.img_path,
 });
 
+const submit = () => {
+  Swal.fire({
+    title: "確定要修改嗎?",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "確定",
+    cancelButtonText: "取消",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      router.put(route('admin.banner.update', props.response.id), item.value, {
+        onSuccess: (response) => {
+          const result = response?.props?.flash?.message ?? {};
+          
+          if (result.res === 'success') {
+            Swal.fire({
+              icon: "success",
+              title: result.msg || "儲存成功",
+              showConfirmButton: false,
+              timer: 1000,
+            }).then(() => {
+              router.get(route('admin.banner.list'));
+            });
+          } else {
+            Swal.fire({
+              icon: "error",
+              title: result.msg || "儲存失敗",
+            });
+          };
+        },
+      });
+    }
+  });
+};
+
 const backBtn = () => router.get(route('admin.banner.list'));
-const updateBtn = () => router.post(route('admin.banner.update', props.response.id), item.value);
 </script>
 
 <template>
@@ -29,7 +64,7 @@ const updateBtn = () => router.post(route('admin.banner.update', props.response.
           </section>
         <section class="flex justify-between">
           <button type="button" @click="backBtn">返回</button>
-          <button type="button" @click="updateBtn">更新資料</button>
+          <button type="button" @click="submit">更新資料</button>
         </section>
     </section>
   </AppLayout>
--- routes/backend.php
+++ routes/backend.php
@@ -3,7 +3,9 @@
 use Inertia\Inertia;
 use App\Models\Banner;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+
 
 // Banner 相關操作
 Route::get('/admin/banner', function () {
@@ -13,6 +15,7 @@ Route::get('/admin/banner', function () {
   ]);
 })->name('admin.banner.list');
 
+
 // 新增頁
 Route::get('/admin/create', function () {
   $banners = Banner::all();
@@ -21,16 +24,36 @@ Route::get('/admin/create', function () {
   ]);
 })->name('admin.banner.create');
 
+
 // 新增資料
 Route::post('/admin/banner', function (Request $request) {
 
-  Banner::create([
-    'title' =>  $request->title,
-    'img_path' => $request->img_path,
-  ]);
-  return redirect(route('admin.banner.create'))->with(['message' => '成功']);
+  try {
+    $request->validate([
+      // 之後再添加
+      'title' => 'required|string|max:255',
+    ]);
 
+    $res = 'success';
+    $message = '儲存成功';
+
+    Banner::create([
+      'title' =>  $request->title,
+      'img_path' => $request->img_path,
+    ]);
+  } catch (\Throwable $th) {
+    Log::info($th->getMessage());
+    $res = 'fail';
+    $message = $th->getMessage();
+  };
+
+  return back()->with(['message' => [
+    'res' => $res,
+    'msg' => $message,
+  ]]);
 })->name('admin.banner.store');
+
+
 
 // 編輯頁
 Route::get('/admin/banner/edit/{id}', function ($id) {
@@ -43,20 +66,58 @@ Route::get('/admin/banner/edit/{id}', function ($id) {
 })->name('admin.banner.edit');
 
 
-// 編輯資料
-Route::post('/admin/banner/update/{id}', function (Request $request, $id) {
-  $banner = Banner::find($id);
+// 更新資料
+Route::put('/admin/banner/update/{id}', function (Request $request, $id) {
 
-  $banner->update([
-    'title' =>  $request->title,
-    'img_path' => $request->img_path,
-  ]);
-  return redirect(route('admin.banner.list'));
+  try {
+    $request->validate([
+      // 之後再添加
+      'title' => 'required|string|max:255',
+    ]);
+
+    $banner = Banner::find($id);
+
+    $res = 'success';
+    $message = '儲存成功';
+
+    $banner->update([
+      'title' =>  $request->title,
+      'img_path' => $request->img_path,
+    ]);
+    if (!$banner) {
+      return back()->with(['message' => [
+        'res' => 'fail',
+        'msg' => '查無此消息',
+      ]]);
+    };
+
+  } catch (\Throwable $th) {
+    Log::info($th->getMessage());
+    $res = 'fail';
+    $message = $th->getMessage();
+  };
+
+  return back()->with(['message' => [
+    'res' => $res,
+    'msg' => $message,
+  ]]);
 })->name('admin.banner.update');
 
+
 // 刪除資料
-Route::get('/admin/banner/delete/{id}', function ($id) {
-  $item = Banner::find($id);
-  $item->delete();
-  return redirect(route('admin.banner.list'));
+Route::delete('/admin/banner/delete/{id}', function ($id) {
+  $banner = Banner::find($id);
+  if (!$banner) {
+    return back()->with(['message' => [
+      'res' => 'fail',
+      'msg' => '查無此消息',
+    ]]);
+  };
+
+  $banner->delete();
+
+  return back()->with(['message' => [
+    'res' => 'success',
+    'msg' => '刪除成功',
+  ]]);
 })->name('admin.banner.delete');
