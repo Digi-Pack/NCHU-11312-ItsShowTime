6da6795903a316b6d5d854fd004a92e02735a779
Angela <111516733+Angela0405@users.noreply.github.com>
2025-05-08 14:36:53 +0800

    fixed: dashboard

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import AppLogoIcon from '@/components/AppLogoIcon.vue';
 </script>
 
 <template>
   <div class="flex aspect-square size-8 items-center justify-center rounded-md ">
-    <AppLogoIcon class="size-5" />
+    <img src="/image/logo-small.webp" alt="">
   </div>
   <div class="ml-1 grid flex-1 text-left text-base">
     <span class="mb-0.5 truncate font-semibold leading-none">特攻服後台</span>
